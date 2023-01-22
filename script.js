88f24a2500593310292fcba796ee43e07bde5703
Tyler-Tolbert <tylertolbert@yahoo.com>
2023-01-21 19:36:45 -0600

    code fixes .3

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function(message) {
+  document.querySelector('.message').textContent = message;
+}
+
 // shows number for testing purposes
 // document.querySelector('.number').textContent = secretNumber;
 
@@ -14,15 +18,16 @@ document.querySelector('.check').addEventListener('click', function () {
   // changes it to a number
   const guess = Number(document.querySelector('.guess').value);
   // adding guess to the console and showing the type.
-  console.log(guess, typeof guess); 
+  // console.log(guess, typeof guess); 
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number!';
+    // document.querySelector('.message').textContent = '⛔️ No number!'; // this shows the message
+    displayMessage('⛔️ No number!');
 
     // when player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
     // shows the random number after correct answer; 
     document.querySelector('.number').textContent = secretNumber;
     // setting the color // it needs to be a string
@@ -36,21 +41,22 @@ document.querySelector('.check').addEventListener('click', function () {
     // when the guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = guess > secretNumber ? '⬆️ Too high!' : '⬇️ Too low!';
+      // checks if its too high or too low
+      displayMessage(guess > secretNumber ? '⬆️ Too high!' : '⬇️ Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
+      displayMessage('💥 You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   } 
 });
 
 
-document.querySelector('.again').addEventListener('click', function() {
+document.querySelector('.again').addEventListener('click', function() { // This resets the game
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
