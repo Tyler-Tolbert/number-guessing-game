32ada99778c6cd8ebeaac7486a7d5087c0424937
Tyler-Tolbert <tylertolbert@yahoo.com>
2023-01-21 18:21:51 -0600

    code fixes .2

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 'use strict';
 
-/* 
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = '🎉Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-
-*/
-
 // selects the random secret number from 1-20
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
